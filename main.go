179262ba6b03fa85673c50e85922a457a1ded7b5
Andrea Envall <andrea.envall@fasttrack-solutions.com>
2022-12-03 12:01:22 +0100

    day three part one

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 
 	one "github.com/andreaenvall/adventOfCode2022/dayOne"
+	three "github.com/andreaenvall/adventOfCode2022/dayThree"
 	two "github.com/andreaenvall/adventOfCode2022/dayTwo"
 )
 
@@ -17,4 +18,8 @@ func main() {
 	puzzle2 := two.PuzzleInput()
 	fmt.Println("Result day 2: ", two.DayTwoPartOne(puzzle2))
 	fmt.Println("Result day 2: ", two.DayTwoPartTwo(puzzle2))
+
+	// Day 3
+	puzzle3 := three.PuzzleInput()
+	fmt.Println("Result day 3: ", three.DayThreePartOne(puzzle3))
 }
